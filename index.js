ec390d23a39e62fa483dbe138ede700861ff1c02
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-02 16:43:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,4 +24,8 @@ function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
     return 'I can\'t hear you';
   }
+  if(string.toUpperCase() === string){
+    return'YES INDEED!';
+  }
+  
 }
